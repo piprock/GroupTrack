911f14bff3d86511371226dd2df4225e09ddf739
piprock <yura.losov@gmail.com>
2025-02-14 19:24:02 +0200

    build file, requirements file

---
+++ build.bat
@@ -0,0 +1,11 @@
+pyinstaller --onefile --noconsole --name GroupTrack src\main.py
+xcopy src\database dist\database\ /E
+copy README.md dist
+copy src\settings.json dist
+copy LICENSE dist
+set name=GroupTrack_v%1
+rename dist "%name%"
+tar -cf "%name%.zip" "%name%"
+rmdir /S /Q build
+rmdir /S /Q %name%
+del GroupTrack.spec
